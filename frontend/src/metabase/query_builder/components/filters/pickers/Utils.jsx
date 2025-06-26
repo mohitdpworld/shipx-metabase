63412c9212bbba53edec65fb6878ffaea12b98ff
Aditya.Gupta <Aditya.Gupta@ext.dpworld.com>
2025-06-26 15:43:01 +0530

    date restriction limit from token

--- frontend/src/metabase/query_builder/components/filters/pickers/Utils.jsx
+++ frontend/src/metabase/query_builder/components/filters/pickers/Utils.jsx
@@ -1,24 +1,11 @@
 // utils/token.js
 
-export const accessLevelToMaxDaysMap = {
-  1: 31,
-  2: 10,
-  3: 7,
-  4: 5,
-  5: 3,
-};
-
 export function getTokenFromURL() {
   const pathParts = window.location.pathname.split("/").filter(Boolean);
-  console.log('pathParts 13', pathParts)
   const isEmbed = pathParts[0] === "embed";
-  console.log('isEmbed 15', isEmbed)
-  console.log('isEmbed 16', isEmbed && pathParts.length >= 3)
   if (isEmbed && pathParts.length >= 3) {
-    console.log('isEmbed 18', isEmbed && pathParts.length >= 3)
     return pathParts[pathParts.length - 1];
   }
-  console.log('isEmbed 21', isEmbed && pathParts.length >= 3)
   return null;
 }
 
@@ -43,44 +30,15 @@ export function decodeJWTPayload(token) {
 export function getMaxRangeDaysFromToken() {
   try {
     const token = getTokenFromURL();
-    console.log('token', token)
     if (!token) return undefined;
 
     const payload = decodeJWTPayload(token);
-    // Uncomment when shipx code is deployed
-    // const isRestricted = Boolean(payload?.userInfo?.dateRestrictedVersion);
-    // if (!isRestricted) return undefined;
-    
-    console.log('payload', payload)
-    const accessLevel = payload?.userInfo?.reportAccessLevel;
-    console.log('accessLevel', accessLevel)
-    console.log('accessLevel', accessLevelToMaxDaysMap[accessLevel] ?? undefined)
+    const maxDaysLimit = payload?.userInfo?.maxDaysLimit;
 
-    return accessLevelToMaxDaysMap[accessLevel] ?? undefined;
+    return maxDaysLimit ?? 93;
   } catch (e) {
     console.warn("Failed to get max range days from token:", e);
     return undefined;
-  }
-}
-
-/**
- * Returns true if dateRestrictedVersion flag is present and true
- */
-
-export function isDateRestrictedVersionEnabled() {
-  try {
-    const token = getTokenFromURL();
-    if (!token) return false;
-
-    const payload = decodeJWTPayload(token);
-    console.log(
-      "Boolean(payload?.userInfo?.dateRestrictedVersion)",
-      Boolean(payload?.userInfo?.dateRestrictedVersion)
-    );
-    return Boolean(payload?.userInfo?.dateRestrictedVersion);
-  } catch (e) {
-    console.warn("Failed to evaluate date restriction flag from token:", e);
-    return false;
   }
 }
 
